2f3e3d0f9b5c5392bf1315d4fd1e46958b395241
Somesh <Somesh@DESKTOP-K6131BB>
2021-08-04 17:49:46 +0530

    upload new all files

---
+++ HelloJava/src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="Somesh";
+		System.out.println(a);
+		
+		System.out.println("Hello World");
+	}
+}
+
+
+
